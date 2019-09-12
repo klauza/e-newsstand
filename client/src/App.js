027276702dfbd599eb51342930acdfc79dad99a1
klauza <klauza.uk@gmail.com>
2019-09-12 12:47:02 +0100

    setting routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+
+import Navigation from './layout/Navigation';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Item from './components/Item';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-
+      <Navigation/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Shop} />
+        <Route exact path="/shop/item/:item" component={Item} />
+        <Route path="/shop?source=hp" component={Shop} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
