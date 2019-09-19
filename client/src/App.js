a0856c338dcdf53ca1f6980ff8425f86e6701f18
klauza <klauza.uk@gmail.com>
2019-09-19 15:28:03 +0100

    import history, add Search route path

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 // import history from './history';
 import './App.css';
+import history from './history';
 
 
 import Navigation from './layout/Navigation/Navigation';
@@ -10,21 +11,24 @@ import Shop from './components/Shop';
 import About from './components/About';
 import About_Wireframes from './components/About_Wireframes';
 import Item from './components/Item';
+import Search from './components/Search';
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
     <div className="App">
       <Navigation/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/shop" component={Shop} />
         <Route exact path="/shop/item/:item" component={Item} />
+        <Route exact path="/shop/search" component={Search} />
         <Route exact path="/about" component={About} />
         <Route exact path="/about/wireframes" component={About_Wireframes} />
       </Switch>
     </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
