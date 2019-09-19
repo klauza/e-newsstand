a3dea2389166385682e426c13e167ba9e5f42ee9
klauza <klauza.uk@gmail.com>
2019-09-19 18:47:10 +0100

    add reusable StyledComponents internal library

---
+++ client/src/layout/StyledComponents.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import history from '../history';
+
+// Styles
+const WrapperStyled = styled.div`
+  width: 70%;
+  margin: 50px auto 50px;
+  padding: 0 10px 50px 10px;
+  background: white;
+  @media(max-width: 998px){ width: 85%; }
+  @media(max-width: 768px){ width: 95%; }
+`;
+const ButtonStyled = styled.button`
+  float: left;
+  padding: 10px;
+  border: 0;
+`;
+const BackButtonStyled = styled.button`
+  float: left;
+  padding: 10px;
+  border: 0;
+`;
+
+
+// Functions
+const goBackOnePage = () => {
+  history.goBack();
+}
+
+
+// Elements
+export const Wrapper = (props) => {
+  return (
+    <WrapperStyled>
+      {props.children}
+    </WrapperStyled>
+  )
+}
+export const Button = (props) => {
+  return(
+    <ButtonStyled>{props.children}</ButtonStyled>
+  )
+}
+export const BackButton = (props) => {
+  return (
+    <BackButtonStyled onClick={goBackOnePage}>{props.children}</BackButtonStyled>
+  )
+}
