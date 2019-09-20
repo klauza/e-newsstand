efbc8d5666d88730d31e7a9b24dc7a05beb583c7
klauza <klauza.uk@gmail.com>
2019-09-20 21:34:17 +0100

    add Display option

--- client/src/components/PopulateWithData.js
+++ client/src/components/PopulateWithData.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addToBasket } from '../actions/basketActions';
@@ -24,14 +24,41 @@ const RightGrid = styled.div`
 
 `;
 const DisplayView = styled.div`
+  height: 50px;
   margin-top: 20px;
+  display: flex; flex-direction: row;
+  align-items: center;
+  span{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 1.5em;
+    margin-top: -10px;
+  }
+  button{
+    height: 100%;
+    margin: 0 10px;
+    border: 0;
+    background: white;
+    outline: 0;
+    i{
+      padding: 10px;
+      transition: all 500ms ease-out;
+      font-size: 2em;
+      &:hover{
+        transition: all 175ms ease;
+        cursor: pointer;
+        color: white;
+        text-shadow: 0px 0px 5px #000000;
+      }
+    }
+  }
 `;
 
 const ItemsWrapper = styled.div`
-  margin: 20px 0;
+  margin: 15px 0 20px;
   width: 100%;
   display: grid; 
   grid-template-columns: repeat(auto-fit, minmax(205px, 1fr));
+  ${props => props.Display ? (`grid-template-columns: repeat(auto-fit, minmax(205px, 1fr))`) : (`grid-template-columns: 1fr`)}
   grid-auto-rows: auto;
   grid-column-gap: 10px;
   grid-row-gap: 35px;
@@ -41,6 +68,7 @@ const ItemsWrapper = styled.div`
   }
   @media(max-width: 768px){
     grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    ${props => props.Display ? (`grid-template-columns: repeat(auto-fit, minmax(150px, 1fr))`) : (`grid-template-columns: 1fr`)}
     grid-column-gap: 5px;
     grid-row-gap: 20px;
   }
@@ -49,8 +77,9 @@ const ItemsWrapper = styled.div`
 const Item = styled.div`
   height: 100%;
   display: grid;
-  grid-template-columns: auto;
-  grid-template-rows: auto;
+  grid-template-columns: ${props => props.Display ? ('1fr') : ('1fr 1fr') };
+  /* grid-template-columns: 1fr; */
+  grid-template-rows: ${props => props.Display ? ('auto') : ('90px 20px 90px') };
   border: 1px solid grey;
   box-shadow: 0px 4px 4px -3px rgba(0,0,0,0.6);
   transition: box-shadow 175ms ease, border 175ms ease;
@@ -65,10 +94,16 @@ const ItemName = styled.h4`
   text-align: center;
   color: black;
   font-weight: 700;
+  grid-column: ${props => props.Display ? (null) : ("2 / 3")};
+  grid-row: ${props => props.Display ? (null) : ("1 / 2")};
+
 `;
 const ItemImage = styled.div`
   width: 100%; height: 100%;
-  padding: 0 5px;
+  padding: ${props => props.Display? ("0 5px") : ("0")}; 
+  grid-column: ${props => props.Display ? (null) : ("1 / 2")};
+  grid-row: ${props => props.Display ? (null) : ("1 / 4")};
+
   img{
     width: 100%; height: 100%;
     object-fit: cover;
@@ -77,45 +112,66 @@ const ItemImage = styled.div`
 const ItemPrice = styled.div`
   text-align: center;
   color: red;
+  align-self: flex-end;
+  grid-column: ${props => props.Display ? (null) : ("2 / 3")};
+  grid-row: ${props => props.Display ? (null) : ("2 / 3")};
 `;
 const ItemShortDesc = styled.div`
-  padding: 5px;
+  padding: 5px 10px;
+  align-self: flex-end;
   text-align: center;
   color: grey;
+  grid-column: ${props => props.Display ? (null) : ("2 / 3")};
+  grid-row: ${props => props.Display ? (null) : ("3 / 4")};
 `;
 
 const PopulateWithData = ({shopData, addToBasket}) => {
 
+  const [view, setView] = useState(true); // true=inline || false=blocks
   
 
-  const throwToBasket = (item) => {
-    addToBasket(item);
+  // const throwToBasket = (item) => {
+  //   addToBasket(item);
+  // }
+  {/* <button onClick={()=>throwToBasket(item)}>add to basket</button> */}
+
+  const setViewToInline = () => {
+    if(view !== false){ 
+      setView(false);
+      // update redux
+    }
+  }
+
+  const setViewToBlocks = () => {
+    if(view !== true){ 
+      setView(true);
+      // update redux
+    }
   }
 
 
 
-{/* <button onClick={()=>throwToBasket(item)}>add to basket</button> */}
-
  if(shopData.length > 0){
     return ( 
       <PageWrap>
-        <DetailedSearch><div>detailed search (filter) </div> <br/><hr/><br/> <div>popular items populated vertically- to fill up whitespace</div></DetailedSearch>
+        <DetailedSearch><div>detailed search (filter) </div> <br/><hr/><br/> <div>popular items populated vertically- to fill up whitespace (hide on mobile) </div></DetailedSearch>
         
         <RightGrid>
           <DisplayView>
-            View: 
-            <button>A</button>
-            <button>B</button>
+            <span>View:</span> 
+            <button onClick={setViewToBlocks}><i className="fa fa-th"></i></button>
+            <button onClick={setViewToInline}><i className="fa fa-align-justify"></i></button>
           </DisplayView>
 
-          <ItemsWrapper display={view}>
+          <ItemsWrapper Display={view}>
             {shopData.map(item => 
               <Link key={item.id} to={{ pathname: `/shop/item/${item.id}`}}>
-                <Item>
-                  <ItemName>{item.name}</ItemName>
-                  <ItemImage><img src={item.imgs[0]} alt=""/></ItemImage>
-                  <ItemPrice>£ {item.price}</ItemPrice>
-                  <ItemShortDesc>{item.shortDesc}</ItemShortDesc>
+                <Item Display={view}>
+                  <ItemName Display={view}>{item.name}</ItemName>
+                  <ItemImage Display={view}><img src={item.imgs[0]} alt=""/></ItemImage>
+                  <ItemPrice Display={view}>£ {item.price}</ItemPrice>
+                  <ItemShortDesc Display={view}>{item.shortDesc}</ItemShortDesc>
+                  {/* long desc */}
                 </Item>
               </Link>
             )}
