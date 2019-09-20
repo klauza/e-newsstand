58c8b9d532f6cde7987ac131e4c9a46e4ac61c11
klauza <klauza.uk@gmail.com>
2019-09-20 23:35:39 +0100

    API data update

--- data/shop.js
+++ data/shop.js
@@ -7,35 +7,62 @@ const shopItems = [
       {
         id: 1,
         name: "newspaper-1",
-        slugs: ["paper", "sheet", "card"]
+        longName: "the-longer name goes in here",
+        slugs: ["paper", "sheet", "card"],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 2,
         name: "newspaper-2",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 3,
         name: "newspaper-3",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 4,
         name: "newspaper-4",
+        longName: "the-longer name goes in here",
         slugs: [],
-        featured: true
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 5,
         name: "newspaper-5",
+        longName: "the-longer name goes in here",
         slugs: [],
-        featured: true
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 6,
         name: "newspaper-6",
+        longName: "the-longer name goes in here",
         slugs: [],
-        featured: true
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       }
     ]
    
@@ -47,17 +74,32 @@ const shopItems = [
       {
         id: 7,
         name: "postcard-1",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 8,
         name: "postcard-2",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 9,
         name: "postcard-3",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       }
     ]
   },
@@ -68,17 +110,42 @@ const shopItems = [
       {
         id: 10,
         name: "gadget-1",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 11,
         name: "gadget-2",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       },
       {
         id: 12,
         name: "gadget-3",
-        slugs: []
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+      },
+      {
+        id: 13,
+        name: "the gadget",
+        longName: "the-longer name goes in here",
+        slugs: [],
+        price: 29,
+        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
       }
     ]
   }
