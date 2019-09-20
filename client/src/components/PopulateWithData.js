638e787a389258a44782d68b72de14fc49049968
klauza <klauza.uk@gmail.com>
2019-09-20 16:43:39 +0100

    css: shop listed items page

--- client/src/components/PopulateWithData.js
+++ client/src/components/PopulateWithData.js
@@ -20,70 +20,108 @@ const DetailedSearch = styled.div`
   }
 `;
 
+const RightGrid = styled.div`
+
+`;
+const DisplayView = styled.div`
+  margin-top: 20px;
+`;
+
 const ItemsWrapper = styled.div`
   margin: 20px 0;
   width: 100%;
   display: grid; 
   grid-template-columns: repeat(auto-fit, minmax(205px, 1fr));
-  grid-auto-rows: 300px;
+  grid-auto-rows: auto;
   grid-column-gap: 10px;
   grid-row-gap: 35px;
   justify-content: center;
-
+  a{
+    text-decoration: none;
+  }
   @media(max-width: 768px){
     grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-    grid-auto-rows: 250px;
     grid-column-gap: 5px;
     grid-row-gap: 20px;
   }
 `;
 
 const Item = styled.div`
-  border: 1px solid black;
-  width: 100%; height: 100%;
-  display: block;
-  a{
-    width: 100%; height: 100%;
-    display: block;
-    &:hover{
-      background: lightsalmon;
-    }
+  height: 100%;
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto;
+  border: 1px solid grey;
+  box-shadow: 0px 4px 4px -3px rgba(0,0,0,0.6);
+  transition: box-shadow 175ms ease, border 175ms ease;
+  &:hover{
+    transition: all 175ms ease;
+    border: 1px solid black;
+    box-shadow: 0px 4px 5px -2px rgba(0,0,0,0.7);
   }
+`;
+const ItemName = styled.h4`
+  margin: 10px 0;
+  text-align: center;
+  color: black;
+  font-weight: 700;
+`;
+const ItemImage = styled.div`
+  width: 100%; height: 100%;
+  padding: 0 5px;
+  img{
+    width: 100%; height: 100%;
+    object-fit: cover;
+  }
+`;
+const ItemPrice = styled.div`
+  text-align: center;
+  color: red;
+`;
+const ItemShortDesc = styled.div`
+  padding: 5px;
+  text-align: center;
+  color: grey;
 `;
 
 const PopulateWithData = ({shopData, addToBasket}) => {
 
+  
 
   const throwToBasket = (item) => {
     addToBasket(item);
   }
 
 
+
+{/* <button onClick={()=>throwToBasket(item)}>add to basket</button> */}
+
  if(shopData.length > 0){
     return ( 
       <PageWrap>
         <DetailedSearch><div>detailed search (filter) </div> <br/><hr/><br/> <div>popular items populated vertically- to fill up whitespace</div></DetailedSearch>
-        <ItemsWrapper>
-
-          {shopData.map(item => 
-
-            <Link key={item.id} to={{ pathname: `/shop/item/${item.id}`}}>
-            <Item>
-              
-
-                <div>title</div>
-                <div><img src="" alt=""/></div>
-                <div>price</div>
-                <div>short desc</div>
-                {item.name}
-                {/* <button onClick={()=>throwToBasket(item)}>add to basket</button> */}
-
-            </Item>
-            </Link>
         
-          )}
+        <RightGrid>
+          <DisplayView>
+            View: 
+            <button>A</button>
+            <button>B</button>
+          </DisplayView>
 
-        </ItemsWrapper>
+          <ItemsWrapper display={view}>
+            {shopData.map(item => 
+              <Link key={item.id} to={{ pathname: `/shop/item/${item.id}`}}>
+                <Item>
+                  <ItemName>{item.name}</ItemName>
+                  <ItemImage><img src={item.imgs[0]} alt=""/></ItemImage>
+                  <ItemPrice>£ {item.price}</ItemPrice>
+                  <ItemShortDesc>{item.shortDesc}</ItemShortDesc>
+                </Item>
+              </Link>
+            )}
+          </ItemsWrapper>
+
+        </RightGrid>
       </PageWrap>
     )
   } else{
