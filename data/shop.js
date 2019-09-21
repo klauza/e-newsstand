248fb774d096007c1a248ca0ce0ff58e0b0fdebd
klauza <klauza.uk@gmail.com>
2019-09-21 21:08:10 +0100

    add data

--- data/shop.js
+++ data/shop.js
@@ -12,7 +12,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg", "https://dynamicmedia.zuza.com/zz/m/original_/3/e/3efda65e-00f5-4a32-b2b3-a6de2bdbe401/Independent_promo_photo1_-_web___Super_Portrait.jpg", "https://images.glaciermedia.ca/polopoly_fs/1.23451029.1538586555!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/newspapers.jpg"],
+        inStock: 20
       },
       {
         id: 2,
@@ -22,7 +23,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 0
       },
       {
         id: 3,
@@ -32,7 +34,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 4,
@@ -42,7 +45,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 5,
@@ -52,7 +56,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 6,
@@ -62,7 +67,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       }
     ]
    
@@ -79,7 +85,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 8,
@@ -89,7 +96,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 9,
@@ -99,7 +107,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       }
     ]
   },
@@ -115,7 +124,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 11,
@@ -125,7 +135,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 12,
@@ -135,7 +146,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       },
       {
         id: 13,
@@ -145,7 +157,8 @@ const shopItems = [
         price: 29,
         shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"]
+        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        inStock: 20
       }
     ]
   }
