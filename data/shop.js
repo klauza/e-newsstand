64633f0c1c82ed3447df3cf1c888aa51d6e3356b
klauza <klauza.uk@gmail.com>
2019-10-09 17:35:11 +0100

    add data items for shop

--- data/shop.js
+++ data/shop.js
@@ -6,70 +6,60 @@ const shopItems = [
     [
       {
         id: 1,
-        name: "newspaper-1",
-        longName: "the-longer name goes in here",
-        slugs: ["paper", "sheet", "card"],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg", "https://dynamicmedia.zuza.com/zz/m/original_/3/e/3efda65e-00f5-4a32-b2b3-a6de2bdbe401/Independent_promo_photo1_-_web___Super_Portrait.jpg", "https://images.glaciermedia.ca/polopoly_fs/1.23451029.1538586555!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/newspapers.jpg"],
-        inStock: 20
+        name: "Harry Potter Daily Prophet",
+        longName: "Harry Potter Daily-Prophet newspaper, magazine nr: 334",
+        slugs: ["harry", "potter", "harry potter", "hogwarts", "newspaper", "paper"],
+        price: 59.99,
+        shortDesc: "The Daily Prophet is a wizarding newspaper.",
+        longDesc: "The Daily Prophet is a wizarding newspaper. It is the primary source of news for British and Irish wizards. Because of its ability to influence the minds of many in the British (and probably Irish) wizarding community, the paper has been known to have its content slanted intentionally by the Ministry of Magic.",
+        imgs: ["https://i.pinimg.com/originals/fd/29/5e/fd295e456ceabce16bbc79aa9c85ae6f.jpg", "https://imagesvc.ti-media.net/crop?width=640&height=400&url=https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/57/2016/12/dvwe.jpg&type=auto&quality=75&gravity=north", "https://cinescopia.com/wp-content/uploads/2013/11/SiriusSightedDailyProphet.jpg"],
+        inStock: 6
       },
       {
         id: 2,
-        name: "newspaper-2",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        name: "The New York Times Titanic story",
+        longName: "The New York Times Titanic story newspaper",
+        slugs: ["titanic", "new york times"],
+        price: 39.99,
+        shortDesc: "Read about Titanic story",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 0
+        imgs: ["https://ae01.alicdn.com/kf/HTB1KecxgWagSKJjy0Fhq6ArbFXaf/New-York-Times-Kraft-Paper-Bar-vintage-Poster-Retro-Historical-Moment-Poster-Decorative-Wall-Sticker.jpg", "https://ae01.alicdn.com/kf/HTB1XewIQwHqK1RjSZFEq6AGMXXah/New-York-Time-Newspaper-Vintage-bar-cafe-Decoration-Kraft-Paper-Poster-Retro-Wall-Sticker-Print-Painting.jpg"],
+        inStock: 1
       },
       {
         id: 3,
-        name: "newspaper-3",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        name: "Newspapers collection, 1 random piece",
+        longName: "Random newspaper from XIX century, get it from this collection",
+        slugs: ["newspaper", "collection", "random", "gadget", "paper", "old"],
+        price: 11.99,
+        shortDesc: "Get a random newspaper from XIX century",
+        longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet placeat perspiciatis, iusto distinctio, harum eaque saepe blanditiis sapiente inventore, aperiam maiores dolore recusandae at officiis! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://ae01.alicdn.com/kf/H2fb670b6b0334142beb7c553c85d1ac07/8sheets-Retro-English-Book-Page-Background-Material-Vintage-English-Newspapers-Deco-Sticker-DIY-Scrapbooking-planner-Diary.jpg", "https://ae01.alicdn.com/kf/He98ee3b3d99b4568992dfffb6a0d5ec5T/8sheets-Retro-English-Book-Page-Background-Material-Vintage-English-Newspapers-Deco-Sticker-DIY-Scrapbooking-planner-Diary.jpg", "https://ae01.alicdn.com/kf/H4d97cd4906db49f4bc7d09eb0c92314eE/8sheets-Retro-English-Book-Page-Background-Material-Vintage-English-Newspapers-Deco-Sticker-DIY-Scrapbooking-planner-Diary.jpg"],
+        inStock: 15
       },
       {
         id: 4,
-        name: "newspaper-4",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        name: "Basic newspaper",
+        longName: "Basic 20-page newspaper made of paper for people",
+        slugs: ["normal", "basic", "newspaper", "paper"],
+        price: 3.99,
+        shortDesc: "A newspaper made of unusual paper",
+        longDesc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, at.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
         imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        inStock: 100
       },
       {
         id: 5,
-        name: "newspaper-5",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
-      },
-      {
-        id: 6,
-        name: "newspaper-6",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        name: "Business secrets newspaper",
+        longName: "Business cheatsheet magazine, read about success",
+        slugs: ["money", "rich", "newspaper", "business"],
+        price: 99.99,
+        shortDesc: "Be a rich person after reading this magazine",
+        longDesc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, magnam ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://images.pexels.com/photos/936137/pexels-photo-936137.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/2068975/pexels-photo-2068975.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/327531/pexels-photo-327531.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"],
+        inStock: 12
       }
+
     ]
    
   },
@@ -79,36 +69,36 @@ const shopItems = [
     items: [
       {
         id: 7,
-        name: "postcard-1",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        name: "A standard solid envelope",
+        longName: "Envelope which lasts long, can survive crazy weather",
+        slugs: ["envelope", "small", "thing", "letter", "paper", "card", "note"],
+        price: 0.66,
+        shortDesc: "Basic but robust and sufficient",
+        longDesc: "This amazing envelope is great for everyone, wheter you are a lumberman or a driver of road roller.",
+        imgs: ["https://www.yorkshireenvelopes.com/wp-content/uploads/2016/06/brownr_2_1_1.jpg", "https://cdn11.bigcommerce.com/s-xa92dr7qw8/images/stencil/1280x1280/products/608/4375/C6-kraft-envelope__63921.1561109293.jpg?c=2&imbypass=on"],
+        inStock: 1000
       },
       {
         id: 8,
-        name: "postcard-2",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        name: "Arnold's diary",
+        longName: "Arnold's diary is exclusive set of Arnold's stories ",
+        slugs: ["arnold", "diary", "story", "stories"],
+        price: 12.50,
+        shortDesc: "A fantastic book with exclusive stories",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
+        imgs: ["https://images.pexels.com/photos/2355408/pexels-photo-2355408.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/2903064/pexels-photo-2903064.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"],
         inStock: 20
       },
       {
         id: 9,
-        name: "postcard-3",
-        longName: "the-longer name goes in here",
-        slugs: [],
+        name: "A letter to Harry Potter",
+        longName: "Magical letter to Harry Potter from McGonagall",
+        slugs: ["harry", "potter", "letter", "post", "paper"],
         price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        shortDesc: "The Hogwarts acceptance letter to Harry Potter",
+        longDesc: "The Hogwarts letter is a letter of acceptance of to Hogwarts School of Witchcraft and Wizardry, sent to British wizards and witches prior to their first year at Hogwarts. The Hogwarts letter consists of a note of acceptance from the Deputy Headmaster or Headmistress of Hogwarts. This note is written in green ink and is accompanied by a list of required textbooks and materials for the upcoming school year.",
+        imgs: ["https://vignette.wikia.nocookie.net/harrypotter/images/8/82/HogwartsAcceptanceLetter.png/revision/latest?cb=20121108000526", "https://images2.minutemediacdn.com/image/upload/c_crop,h_842,w_1500,x_0,y_264/f_auto,q_auto,w_1100/v1555000678/shape/mentalfloss/propstoree.jpg"],
+        inStock: 1
       }
     ]
   },
@@ -118,47 +108,58 @@ const shopItems = [
     items: [
       {
         id: 10,
-        name: "gadget-1",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        name: "Paper umbrella",
+        longName: "Paper umbrella, made of amazing robust materials",
+        slugs: ["paper", "umbrella", "robust"],
+        price: 199.99,
+        shortDesc: "Anti-all weather umbrella made of solid paper",
+        longDesc: "This is a great chance for you to acquire this amazing umbrella. It's antimalware external shell will protect you from anything. It's an opportunity for you to be protected for unlimited time.",
+        imgs: ["https://ae01.alicdn.com/kf/HTB1cTJUPVXXXXbuXVXXq6xXFXXXB/Originality-Newspaper-Three-Folding-style-Umbrellas-Rain-Women-Men-High-Quality-Cheap-Fashion-Bumbershoot.jpg", "https://ae01.alicdn.com/kf/HTB1HTBPPVXXXXaeaXXXq6xXFXXXO/Originality-Newspaper-Three-Folding-style-Umbrellas-Rain-Women-Men-High-Quality-Cheap-Fashion-Bumbershoot.jpg", "https://ae01.alicdn.com/kf/HTB1GzyfPVXXXXbLXpXXq6xXFXXXq/Originality-Newspaper-Three-Folding-style-Umbrellas-Rain-Women-Men-High-Quality-Cheap-Fashion-Bumbershoot.jpg"],
+        inStock: 5
       },
       {
         id: 11,
-        name: "gadget-2",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        name: "Christopher Columbus navigation set",
+        longName: "Christopher Columbus navigation gadgets - instruments needed to be a captain",
+        slugs: ["navigation", "gadgets", "instruments", "captain", "map", "set", "maps", "adventure", "journey"],
+        price: 119.19,
+        shortDesc: "Are you prepared to become a real Columbus?",
+        longDesc: "Christopher Columbus was an Italian explorer, navigator, and colonist who completed four voyages across the Atlantic Ocean under the auspices of the Catholic Monarchs of Castile and Aragon. While pursuing a route to the Far East, he discovered a viable sailing route to the Americas, then unknown to the Old World. He led the first European expeditions to the Caribbean, Central America, and South America, initiating the opening of the New World for conquest and settlement by Europeans and the permanent European colonization of the Americas Columbus's early life is somewhat obscure, but scholars generally agree that he was born in the Republic of Genoa and spoke a dialect of Ligurian as his first language. He went to sea at a young age and travelled widely, as far north as the British Isles (and possibly Iceland) and as far south as what is now Ghana. He married Portuguese noblewoman Filipa Moniz Perestrelo and was based in Lisbon for several years, but later took a Castilian mistress; he had one son with each woman. Though largely self-educated, Columbus was widely read in geography, astronomy, and history. He formulated a plan to seek a western sea passage to the East Indies, hoping to profit from the lucrative spice trade.",
+        imgs: ["https://www2.freeseek.org/img/products/74247-vintage-retro-damga-washi-bant-yaratc-diy-ev-dekorasyon-guenluegue-oegrenci-icin-kraft-kagt-dekoratif-bant.jpg", "https://ae01.alicdn.com/kf/Hc0dd8134b40b456483d6de394da3ccbfq/Handheld-10X-Magnifying-Glass-Portable-Magnifier-Retro-Handle-Eye-Loupe-Glass-60-70-80-90mm.jpg", "https://ae01.alicdn.com/kf/HLB1ndlwOgHqK1RjSZJnq6zNLpXa6/8M-Creative-DIY-Paper-Wall-Sticker-Funny-Newspaper-World-Map-Printted-Vintage-Pattern-3D-Home-Decor.jpg"],
+        inStock: 0
       },
       {
         id: 12,
-        name: "gadget-3",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        name: "Set of world maps",
+        longName: "This is a set of world maps in various languages and various locations",
+        slugs: ["set", "map", "maps", "world", "paper", "list", "gadgets", "gadget"],
+        price: 99.99,
+        shortDesc: "It is necessary to understand the world",
         longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        imgs: ["https://images.pexels.com/photos/2859169/pexels-photo-2859169.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/697662/pexels-photo-697662.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/2305765/pexels-photo-2305765.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"],
+        inStock: 50
       },
       {
         id: 13,
-        name: "the gadget",
-        longName: "the-longer name goes in here",
-        slugs: [],
-        price: 29,
-        shortDesc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
-        imgs: ["https://www.historic-newspapers.co.uk/images/newspapers/lrg-newspaper.jpg"],
-        inStock: 20
+        name: "Paper animal",
+        longName: "A piece of random animal made of paper",
+        slugs: ["random", "piece", "gadget", "animal"],
+        price: 4.99,
+        shortDesc: "Extraordinary papercraft animal figure",
+        longDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, dolorum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in tortor quis tellus molestie sodales. Mauris tristique auctor orci, et ultrices turpis placerat ut. Praesent.",
+        imgs: ["https://images.pexels.com/photos/40906/art-attachment-background-boat-40906.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"],
+        inStock: 150
+      },
+      {
+        id: 14,
+        name: "Food to read",
+        longName: "Food catering",
+        slugs: ["food", "eating", "eat", "catering"],
+        price: 9.99,
+        shortDesc: "Read our products and eat our food in the same time",
+        longDesc: "A delicious, spicy blend, packed full of iron and low fat to boot. It's ready in under half an hour or can be made in a slow cooker...",
+        imgs: ["https://images.pexels.com/photos/1437760/pexels-photo-1437760.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"],
+        inStock: 999
       }
     ]
   }
