646095b12f9a395bcc5425f11998311d16a1a679
klauza <klauza.uk@gmail.com>
2019-10-13 18:56:42 +0100

    minor css styles for Basket component

--- client/src/components/Basket/Basket.js
+++ client/src/components/Basket/Basket.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { updateBasket, deleteItem } from '../../actions/basketActions';
 import styled from 'styled-components';
@@ -11,6 +11,7 @@ const Container = styled.div`
   margin: 0 auto;
   display: flex; flex-direction: column;
   text-align: center;
+  font-family: 'Oswald', sans-serif;
   i{
     font-size: 6em;
   }
@@ -19,14 +20,13 @@ const ItemsWrapper = styled.div`
   display: grid;
   grid-auto-rows: 200px;
   grid-row-gap: 5px;
-  border: 1px solid red;
   margin-top: 15px;
 `;
 
 // ITEM
 const Item = styled.div`
   position: relative;
-  border: 1px solid black;
+  border: 1px solid black; border-radius: 2px;
   display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 200px;
 `;
 const ItemImage = styled.div`
@@ -42,28 +42,56 @@ const ItemDesc = styled.div`
   flex-direction: column;
   justify-content: space-between;
   padding: 15px;
+
+  button{
+    margin-left: 3px;
+    background: white; color: black;
+    border: 1px solid black;
+    border-radius: 5px;
+    box-shadow: 0 0px 3px -1px black;
+    width: 25px; height: 25px;
+    i{
+      line-height: 25px;
+      font-size: 1em;
+    }
+    &:hover{
+      cursor: pointer;
+      box-shadow: 0 0px 4px 0px black;
+    }
+    &:disabled{
+      color: grey;
+    }
+  }
 `;
-const ButtonUp = styled.button`
-  width: 25px; height: 25px;
-  background: red;
-`;
-const ButtonDown = styled.button`
-  width: 25px; height: 25px;  
-  background: orange;
-`;
+
 const DeleteItem = styled.button`
   position: absolute;
-  top: 50%; right: 0; 
+  top: 50%; right: 5px; 
   transform: translateY(-50%);
+  background: red;
+  width: 30px;
+  height: 30px;
+  box-shadow: 0 0px 3px -1px black;
+  color: white;
+  border: 1px solid white;
+  &:hover{
+    cursor: pointer;
+    box-shadow: 0 0px 4px 0px black;
+  }
+
 `;
 // PAYMENT
 const PayButton = styled.button`
   width: 100px; height: 40px; margin: 25px auto 0;
   border: 1px solid black; border-radius: 3px;
-  background: yellow;
-  color: black;
-  box-shadow: 0 3px 2px -2px #000;
+  background: rgba(45,98,110,0.9);
+  color: white;
+  box-shadow: 0 3px 3px -3px #000;
+  font-family: 'Oswald', sans-serif;
+  font-size: 1.2rem;
+
   &:hover{
+    box-shadow: 0 3px 2px -2px #000;
     cursor: pointer;
   }
 `;
@@ -113,10 +141,10 @@ const Basket = ({updateBasket, deleteItem, basket: {items}}) => {
                     <ItemDesc>
                       <div>{item.name}</div>
                       <div>quantity {item.quantity} 
-                        <ButtonUp disabled={item.inStock <= item.quantity} onClick={()=>{ updtItem({...item, quantity: item.quantity+1})  }}>+</ButtonUp>
-                        <ButtonDown disabled={item.quantity === 1} onClick={()=>{ updtItem({...item, quantity: item.quantity-1})  }}>-</ButtonDown> 
+                        <button disabled={item.inStock <= item.quantity} onClick={()=>{ updtItem({...item, quantity: item.quantity+1})  }}><i className="fa fa-plus"></i></button>
+                        <button disabled={item.quantity === 1} onClick={()=>{ updtItem({...item, quantity: item.quantity-1})  }}><i className="fa fa-minus"></i></button> 
                       </div>
-                      <div>£ {calculatePrice(item.price, item.quantity)}</div>
+                      <div>£ {calculatePrice(item.price, item.quantity)} <span style={{display: "block"}}>{item.quantity > 1 ? (`(£ ${item.price} for one)`) : (null)}</span></div>
                     </ItemDesc>
                  
                     <DeleteItem onClick={()=>delItem(item.id)}> X </DeleteItem>
