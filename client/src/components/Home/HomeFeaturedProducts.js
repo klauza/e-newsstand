6f4b24e08f72370bb5cb0818a12d87b3fed4ce05
klauza <klauza.uk@gmail.com>
2019-10-14 23:08:35 +0100

    create concept to display featured products

--- client/src/components/Home/HomeFeaturedProducts.js
+++ client/src/components/Home/HomeFeaturedProducts.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import styled from 'styled-components';
+import { ChildWrapper } from '../../layout/StyledComponents';
 
 const FeaturedWrapper = styled.div`
   display: flex; flex-direction: column;
@@ -13,48 +14,144 @@ const FeaturedTitle = styled.div`
     color: red;
   }
 `;
-
 const FeaturedBody = styled.div`
   display: grid;
-  grid-template-columns: 50px 1fr 50px;
+  grid-template-columns: 50px 1fr 50px; /* button body button */
   grid-auto-rows: 200px;
+  @media(max-width: 768px){
+    grid-template-columns: 1fr; /* no buttons, just body. Scrolling with finger */
+    overflow-x: scroll;
+      transition: transform 175ms ease;
+    &.moveActive{
+      transition: transform 175ms ease;
+      transform: scale(0.95);
+    }
+  }
 `;
 const FeaturedArrow = styled.div`
   border: 1px solid yellow;
-  button{display: grid; align-items: center;
-    width: 100%; height: 100%;
-  }
-`;
-const FeaturedProducts = styled.div`
-  display: flex; flex-direction: row;
-  div{
+  button{
     display: grid; align-items: center;
-    border: 1px solid black;
     width: 100%; height: 100%;
   }
+  @media(max-width: 768px){
+    display: none;
+  }
 `;
+
+// Content
+const FeaturedProducts = styled.div`
+  display: grid; grid-template-columns: repeat(5, minmax(135px, 200px));
+  justify-content: center;
+  @media(max-width: 768px){
+    grid-template-columns: repeat(5, minmax(135px, 200px));
+    justify-content: left;
+  }
+`;
+const Card = styled.div`
+  display: grid; grid-template-columns: 1fr;
+  margin: 0 10px;
+  @media(max-width: 998px){
+    margin: 0 2.5px;
+  }
+  grid-template-rows: 1fr 1fr;
+  align-items: center;
+  border: 1px solid black;
+  width: 100%p; height: 100%;
+
+  img{
+    width: 100%; height: 100%; object-fit: cover;
+  }
+`;
+const CardItemDesc = styled.div`
+  height: 100%;
+  display: grid; grid-template-rows: 1fr 1fr 1fr;
+  border: 1px solid black;
+`;
+const CardItemName = styled.div`
+  border: 1px solid yellow;
+`;
+const CardItemValue = styled.div`
+
+  border: 1px solid red;
+  div{
+    border: 1px solid orange;
+  }
+`;
+
 
 const HomeFeaturedProducts = () => {
+
+  // const [fetched, setFetched] = useState(false);
+  const theBody = useRef();
+
+  // need photos to display (6? => 2 x 3)
+  // fetch only 6, no more
+
+  const movingBody = () => {
+    theBody.current.classList.add('moveActive');
+  }
+  const notMovingBody = () => {
+    theBody.current.classList.remove('moveActive');
+  }
+
   return (
-    <FeaturedWrapper>
-      <FeaturedTitle><h3>Featured products</h3></FeaturedTitle>
+    <ChildWrapper>
+      <FeaturedWrapper>
+        <FeaturedTitle><h3>Featured products</h3></FeaturedTitle>
 
-      <FeaturedBody>
-        <FeaturedArrow><button>left</button></FeaturedArrow>
+        <FeaturedBody ref={theBody} onTouchStart={movingBody} onTouchEnd={notMovingBody}>
+          <FeaturedArrow><button>left</button></FeaturedArrow>
 
-        <FeaturedProducts>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-        </FeaturedProducts>
+          <FeaturedProducts>
+            <Card>
+              <img src="https://i.ebayimg.com/images/g/XesAAOSwLtZdo4mP/s-l1600.jpg" alt=""/>
+              <CardItemDesc>
+                <CardItemName>Nazwa</CardItemName>
+                <CardItemValue>Price 20$</CardItemValue>
+                <CardItemValue>inStock</CardItemValue>
+              </CardItemDesc>
+            </Card>
+            <Card>
+              <img src="https://i.ebayimg.com/images/g/XesAAOSwLtZdo4mP/s-l1600.jpg" alt=""/>
+              <CardItemDesc>
+                <CardItemName>Nazwa</CardItemName>
+                <CardItemValue>Price 20$</CardItemValue>
+                <CardItemValue>inStock</CardItemValue>
+              </CardItemDesc>
+            </Card>
+            <Card>
+              <img src="https://i.ebayimg.com/images/g/XesAAOSwLtZdo4mP/s-l1600.jpg" alt=""/>
+              <CardItemDesc>
+                <CardItemName>Nazwa</CardItemName>
+                <CardItemValue>Price 20$</CardItemValue>
+                <CardItemValue>inStock</CardItemValue>
+              </CardItemDesc>
+            </Card>
+            <Card>
+              <img src="https://i.ebayimg.com/images/g/XesAAOSwLtZdo4mP/s-l1600.jpg" alt=""/>
+              <CardItemDesc>
+                <CardItemName>Nazwa</CardItemName>
+                <CardItemValue>Price 20$</CardItemValue>
+                <CardItemValue>inStock</CardItemValue>
+              </CardItemDesc>
+            </Card>
+            <Card>
+              <img src="https://i.ebayimg.com/images/g/XesAAOSwLtZdo4mP/s-l1600.jpg" alt=""/>
+              <CardItemDesc>
+                <CardItemName>Nazwa</CardItemName>
+                <CardItemValue>Price 20$</CardItemValue>
+                <CardItemValue>inStock</CardItemValue>
+              </CardItemDesc>
+            </Card>
+          </FeaturedProducts>
 
-        <FeaturedArrow><button>right</button></FeaturedArrow>
-      </FeaturedBody>
+          <FeaturedArrow><button>right</button></FeaturedArrow>
+        </FeaturedBody>
 
 
-    </FeaturedWrapper>
+      </FeaturedWrapper>
+    </ChildWrapper>
   )
 }
 
