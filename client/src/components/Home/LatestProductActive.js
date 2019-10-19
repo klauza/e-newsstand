68470ff69c139d33b0ddecb76ecd8c33473f202c
klauza <klauza.uk@gmail.com>
2019-10-19 14:52:04 +0100

    home latestProducts layout

--- client/src/components/Home/LatestProductActive.js
+++ client/src/components/Home/LatestProductActive.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import posed, { PoseGroup } from 'react-pose'
 
-
-const Product = styled.div`
+const ProductProps = posed.div({
+  enter: { opacity: 1, y: 0, delay: 250 },
+  exit: { opacity: 0, y: -25, transition: { duration: 600 } }
+})
+const Product = styled(ProductProps)`
   display: grid; grid-template-columns: repeat(10, 1fr);
   grid-auto-rows: 1fr;
+  position: absolute;
+  top: 200px; 
+  margin-bottom: 150px;
+  width: 100%; 
   /* grid-template-rows: repeat(9, 40px); */
 
   &::before {
@@ -20,59 +28,115 @@ const Product = styled.div`
   }
 `;
 
-const BlackBox1 = styled.div`
-  grid-column: 3 / 5;
-  grid-row: 1 / span 2;
-  background: #000;
-`;
-const BlackBox2 = styled.div`
-  grid-column: 5 / 7;
-  grid-row: 9 / span 2;
-  background: #000;
-`;
 
-const ProductImgOne = styled.div`
-  grid-column: 4 / 9;
-  grid-row: 2 / span 5;
-  width: 100%; height: 100%;
-  border: 2px solid red;
-  img{
-    width: 100%; height: 100%; object-fit: cover;
+
+
+
+
+
+const ImgTwoProps = posed.div({
+  zoomedIn: {
+    position: 'fixed',
+    top: 60,
+    left: 20,
+    bottom: 60,
+    right: 20,
+    flip: true,
+    duration: 400,
+    ease: [0.08, 0.69, 0.2, 0.99]
+  },
+  zoomedOut: {
+    position: 'static',
+    width: 'auto',
+    height: 'auto',
+    flip: true,
+    duration: 400,
+    ease: [0.08, 0.69, 0.2, 0.99]
   }
-`;
-const ProductImgTwo = styled.div`
-  grid-column: 1 / 6;
-  grid-row: 5 / span 5;
+  
+})
+const ProductImg = styled(ImgTwoProps)`
+  grid-column: 3 / 9;
+  grid-row: 2 / span 6;
   width: 100%; height: 100%;
   border: 2px solid purple;
   img{
     width: 100%; height: 100%; object-fit: cover;
   }
 `;
-const ProductDesc = styled.div`
-  grid-column: 7 / span 4;
-  grid-row: 8 / span 3;
+
+const ProductOneProps = posed.div({
+  enter: { x: 0, opacity: 1, delay: 220 },
+  exit: { x: 150, opacity: 0 }
+})
+const ProductDescName = styled(ProductOneProps)`
+  grid-row: 8;
+  grid-column: 4 / span 4;
   width: 100%; height: 100%;
   border: 2px solid orangered;
   text-align: center;
-
-  display: grid;
-  grid-template-rows: repeat(3, 1fr);
 `;
 
+const ProductLinkProps = posed.div({
+  enter: { x: 0, opacity: 1, delay: 220 },
+  exit: { x: -150, opacity: 0 }
+})
+const ProductDescLink = styled(ProductLinkProps)`
+  grid-row: 9;
+  grid-column: 4 / span 4;
+  width: 100%; height: 100%;
+  border: 2px solid orangered;
+  text-align: center;
+`;
+
+const ProductDescProps = posed.div({
+  enter: { x: 0, opacity: 1, delay: 220 },
+  exit: { x: 150, opacity: 0 }
+})
+const ProductDescDesc = styled(ProductDescProps)`
+  grid-row: 10;
+  grid-column: 4 / span 4;
+  width: 100%; height: 100%;
+  border: 2px solid orangered;
+  text-align: center;
+`;
+const BottomPanel = styled.div`
+  grid-row: 11 / span 2;
+  grid-column: 3 / span 6;
+  border: 1px solid blue; border-radius: 30%;
+  background: blue;
+`;
 const LatestProductActive = ({activeProduct}) => {
+
+  const [isZoomed, setIsZoomed] = useState(false);
+
+
+
+
   return (
-    <Product>
-      <BlackBox1></BlackBox1>
-      <ProductImgOne><img src={activeProduct.img1} alt=""/></ProductImgOne>
-      <ProductImgTwo><img src={activeProduct.img2} alt=""/></ProductImgTwo>
-      <ProductDesc>
-        <p>{activeProduct.name}</p>
-        <a href={activeProduct.link}>Check this out</a>
-        <p>{activeProduct.desc}</p>
-      </ProductDesc>
-      <BlackBox2></BlackBox2>
-    </Product>
+
+    <PoseGroup flipMove={false}>
+      <Product key={activeProduct.id}>
+
+
+        <ProductImg pose={isZoomed ? "zoomedIn" : "zoomedOut"} onClick={()=>setIsZoomed(prevState => !prevState)}><img src={activeProduct.img} alt=""/></ProductImg>
+        
+        <ProductDescName>
+          <p>{activeProduct.name}</p>
+        </ProductDescName>
+
+        <ProductDescLink>
+          <a href={activeProduct.link}>Check this out</a>
+        </ProductDescLink>
+
+        <ProductDescDesc>
+          <p>{activeProduct.desc}</p>
+        </ProductDescDesc>
+        <BottomPanel></BottomPanel>
+        
+      </Product>
+    </PoseGroup>
+
   )
 }
 
