40539e1cc44b4c9842623c04cd28298407a6f6b4
klauza <klauza.uk@gmail.com>
2019-10-21 18:55:52 +0100

    replace gif spinner with svg one

--- client/src/layout/Loader.js
+++ client/src/layout/Loader.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import loadingImg from '../media/smallloader.gif';
 import styled from 'styled-components';
+import SvgSpinner from '../Icons/Spinner';
 
 const LoaderWrapper = styled.div`
   width: 200px; height: 200px;
   margin: 50px auto;
-  img{
-    width: 100%; height: 100%; object-fit: cover;
+
+  svg{
+    display: block;
+    margin: 0 auto;
+    animation: svg-animation infinite 750ms linear;
+
+    @keyframes svg-animation{
+      0%{
+        transform: rotate(0deg);
+      }
+      100%{
+        transform: rotate(360deg);
+      }
+    }
   }
 `;
 
 const Loader = () => {
   return (
     <LoaderWrapper>
-      <img src={loadingImg} alt=""/>
+      <SvgSpinner />
     </LoaderWrapper>
   )
 }
