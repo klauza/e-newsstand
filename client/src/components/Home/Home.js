52e2ce65d4877c18660a3d06be125bc2d2211e0b
klauza <klauza.uk@gmail.com>
2019-10-29 13:36:06 +0000

    add button on home page to access admin panel

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,28 @@
 import React, {useEffect, createRef} from 'react';
+import { Link } from 'react-router-dom';
 import HomeHero from './HomeHero';
 import HomeFeaturedProducts from './HomeFeaturedProducts/HomeFeaturedProducts';
 import LatestProducts from './LatestProducts';
 import AboutUs from './AboutUs';
 import HomeCards from './HomeCards';
 import HomeCards2 from './HomeCards2';
+import styled from 'styled-components';
 
+const Button = styled.button`
+  position: absolute;
+  left: 50%; transform: translateX(-50%);
+  top: 2.5%;
+  z-index: 15;
+  border: 2px solid white;
+  background: black;
+  color: white;
+  padding: 7px;
+  font-family: sans-serif;
+  cursor: pointer;
+  box-shadow: 0 2px 2px -1px #000;
+  outline: 1px solid white;
+  outline-offset: 7.5px;
+`;
 
 const Home = () => {
 
@@ -47,6 +64,7 @@ const Home = () => {
   
   return (
     <div style={{position: "absolute", left: "0", right: "0"}}>
+      <Link to="/admin"><Button style={{}}>Admin panel</Button></Link>
       <HomeHero refs={refs} handleClick={handleClick} anchor={anchors[0]} />
       {/* <HomeCards /> */}
       <HomeCards2 />
