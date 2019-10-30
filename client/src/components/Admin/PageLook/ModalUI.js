855fc42ebaa7c529f122a4cb3bd86d8b0b97bb4c
klauza <klauza.uk@gmail.com>
2019-10-30 17:03:27 +0000

    Modal ui-sections on Admin page

--- client/src/components/Admin/PageLook/ModalUI.js
+++ client/src/components/Admin/PageLook/ModalUI.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import posed from 'react-pose';
 
 const ModalContainer = styled.div`
   box-shadow: 0 -3px 4px -1px rgba(0,0,0,0.35); 
@@ -40,54 +41,90 @@ const Header = styled.h2`
 display: inline-block;
   text-align: center;
 `;
+// ----------------------------------
 
-const Grid = styled.div`
+
+const GridPosed = posed.div({
+  open: {
+    delayChildren: 500, 
+    staggerChildren: 50
+  }
+})
+const Grid = styled(GridPosed)`
   width: 200px;
   margin: 25px auto;
   display: flex; flex-direction: column;
-  div{
-    display: grid; 
-    align-items: center; justify-items: center;
-    width: 100%;
-    height: 100px;
-    border: 1px solid black;
-    font-size: 1.75em;
-    &:hover{
-      cursor: pointer;
-      background: white;
-    }
-  }
 `;
 
-const ModalUI = (props) => {
+const SectionPose = posed.section({
+  open: { y: 0, opacity: 1 },
+  closed: { y: 20, opacity: 0 }
+})
+const Section = styled(SectionPose)`
+  background-image: url(${props => props.backgroundImg});
+  display: grid; 
+  align-content: space-between; justify-items: center;
+  width: 100%;
+  height: 100px;
+  border: 1px solid black;
+  position: relative;
+  span{
+    font-size: 0.9em;
+  }
+  button{
+    cursor: pointer;
+    padding: 2.5px;
+  }
+  &:hover{
+    cursor: default;
+    background-color: rgba(255,255,255,0.25);
+  }
 
+/* 
+  &.active {
+    background: grey;
+  } */
+`;
+
+
+const ModalUI = (props) => {
   const [closeModal, setCloseModal] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const render = [];
 
-  const renderSections = () => {
-    for (let i=0; i < props.modal.sectionQty; i++){
-      render.push(<div><i className="fa fa-plus-circle"></i></div>);
-    }
-    return render
-  }
+  React.useEffect(()=>{
+    setIsOpen(true);
+  }, [])
 
   const exitModal = () => {
     setCloseModal(true);
-
     setTimeout(()=>{
       props.setShowModal(null)
     }, 500)
   }
+
+  const sectionHandleClick = (e) => {
+    // set active section
+    // Array.from(e.target.parentNode.children).forEach(sec => sec.classList.remove('active'));
+    // e.target.classList.add('active');
+
+    // next
+  }
+
   return (
     <ModalContainer closeModal={closeModal}>
       <button onClick={exitModal}>Close</button>
       <button onClick={exitModal}>Confirm changes</button>
       <Header>Choose section for {props.modal.name} </Header>
 
-      <Grid>
+      <Grid pose={isOpen ? 'open' : 'closed'}>
       
-      {renderSections()}
-
+        {props.modal.sections.map((section,id)=>
+          <Section backgroundImg={section.img} key={id} onClick={(e)=>sectionHandleClick(e)}>
+            <span>{section.name}</span>
+            <button>Edit</button>
+          </Section>
+        )}
       </Grid>
 
 
