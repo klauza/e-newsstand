e2824bb0cc41b18ea1725571c1352520634e717d
klauza <klauza.uk@gmail.com>
2019-10-30 17:02:36 +0000

    adjust layout of modal page, add temp images

--- client/src/components/Admin/PageLook/AdminUI.js
+++ client/src/components/Admin/PageLook/AdminUI.js
@@ -1,35 +1,57 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import posed from 'react-pose';
 import { Wrapper } from '../../../layout/StyledComponents';
 import ModalUI from './ModalUI';
+import { homeImage0, homeImage1, homeImage2, homeImage3, homeImage4, homeImage5 } from '../../../media/index';
 
 
 const Container = styled.div`
   width: 100%;
   text-align: center;
-  h1{
-      font-family: sans-serif;
-  }
+
   section{
-    width: 100%; margin: 25px auto 0;
+    width: 100%; margin: 0 auto;
     border-bottom: 1px solid black;
+    display: grid; 
+    grid-template-columns: auto auto 200px 200px;
+    grid-template-areas: "change-content . name endpoint";
+    /* justify-content: center; */
+
+    span{
+      border-left: 1px solid black;
+      border-right: 1px solid black;
+      
+      padding: 10px;
+      font-family: sans-serif;
+      /* font-size: 2.15rem; */
+      text-align: center;
+      height: 100%;
+      line-height: 50px;
+    }
+    .change-content{
+      grid-area: change-content;
+      width: auto;
+      pointer-events: ${props => props.disableClick ? ('none') : ('auto')};
     cursor: pointer;
-    &:hover{
+      &:hover{
       background: lightgrey;
     }
-    div{
-      padding: 10px;
-      display: flex; flex-direction: row;
-      justify-content: center;
     }
-
-    span, h2{
-      padding: 5px;
-      font-family: sans-serif;
-      font-size: 2.15rem;
-      text-align: center;
+    .name{
+      grid-area: name;
+    }
+    .endpoint{
+      grid-area: endpoint;
     }
   }
+`;
+
+
+const Header = styled.h1`
+  font-family: sans-serif;
+  margin-bottom: 25px;
+  opacity: 0.55;
 `;
 
 const linkDatabase = [
@@ -37,61 +59,80 @@ const linkDatabase = [
     id: 0, 
     name: "Home page", 
     endpoint: `"/"`,
-    sectionQty: 5
+    sections: [
+      {id: 0, name: 'Hero', img: homeImage0},
+      {id: 1, name: 'Trust-Boxes', img: homeImage1},
+      {id: 2, name: 'Featured-products', img: homeImage2},
+      {id: 3, name: 'About', img: homeImage3},
+      {id: 4, name: 'Newest-products', img: homeImage4}
+    ]
   },
   {
     id: 1, 
     name: "Shop", 
     endpoint: `"/shop"`,
-    sectionQty: 1
+    sections: [
+      {id: 0, name: 'Main', img: "shop-image-0.png"}
+    ]
   },
   {
     id: 2, 
     name: "Shop-search", 
     endpoint: `"/shop/search..."`,
-    sectionQty: 0
+    sections: [
+      {}
+    ]
   },
   {
     id: 3, 
     name: "Item", 
     endpoint: `"/shop/item"`,
-    sectionQty: 2
+    sections: [
+      {id: 0, name: 'Gallery', img: "item-image-0.png"},
+      {id: 1, name: 'Description', img: "item-image-1.png"}
+    ]
   },
   {
     id: 4, 
     name: "Contact", 
     endpoint: `"/contact"`,
-    sectionQty: 0
+    sections: [
+      {}
+    ]
   },
   {
     id: 5, 
     name: "Basket", 
     endpoint: `"/basket"`,
-    sectionQty: 1
+    sections: [
+      {id: 0, name: 'Main', img: "basket-image-0.png"}
+    ]
   }
 ];
+
 
 const AdminUI = () => {
 
 
   const [showModal, setShowModal] = useState(null);
 
+
   console.log(showModal);
+  
   return (
     <React.Fragment>
-
+      
       {(showModal !== null) && <ModalUI setShowModal={setShowModal} modal={showModal} />}
 
       <Wrapper>
-        <Container>
-          <h1>Choose page you'd like to change</h1>
+        <Container disableClick={showModal !== null}>
+          <Header r={100} >Choose page you'd like to change</Header>
 
           {linkDatabase.map((section, id) => 
-          <section key={id} onClick={()=>setShowModal(section)}>
-            <div>
-              <h2>{section.name}</h2>
-              <span>{section.endpoint}</span>
-            </div>
+          <section key={id}>
+            <span className="change-content" onClick={()=>setShowModal(section)}>change content</span>
+            <span className="name">{section.name}</span>
+            <span className="endpoint">{section.endpoint}</span>
           </section>
           )}
 
--- client/src/media/index.js
+++ client/src/media/index.js
@@ -7,3 +7,10 @@ export { default as HeroImage } from './Home/pexels-photo-2676587.jpg';
 export { default as newspapersImage } from './ShopCategories/newspapersImage.jpg';
 export { default as lettersImage } from './ShopCategories/lettersImage.jpg';
 export { default as gadgetsImage } from './ShopCategories/gadgetsImage.jpg';
+
+// Admin-change-ui-sections
+export {default as homeImage0} from './Admin/home-image-0.png';  
+export {default as homeImage1} from './Admin/home-image-1.png';  
+export {default as homeImage2} from './Admin/home-image-2.png';  
+export {default as homeImage3} from './Admin/home-image-3.png';  
+export {default as homeImage4} from './Admin/home-image-4.png';  
