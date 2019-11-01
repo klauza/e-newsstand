61cbf9d3b70c5254ee66da7357ccfd1a500efc26
klauza <klauza.uk@gmail.com>
2019-11-01 13:44:39 +0000

    Admin: CategoryModal adjusted animations & css

--- client/src/components/Admin/Stock/AdminCategoryModal.js
+++ client/src/components/Admin/Stock/AdminCategoryModal.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import posed from 'react-pose';
+import EditItemModal from './EditItemModal';
 
-
-const Main = styled.div`
+const MainPosed = posed.div({
+  initialPose: 'closed',
+  open: {
+    opacity: 1,
+  },
+  closed: {     
+    opacity: 0
+  }
+})
+const Main = styled(MainPosed)`
   position: fixed;
   top: 0px; left: 0px;
   right: 0px;
@@ -28,27 +37,32 @@ const ContentPosed = posed.div({
 const Content = styled(ContentPosed)`
   display: flex; flex-direction: column;
   align-self: flex-start; justify-content: flex-start;
-  width: 90vw; height: 100%;
+  width: 90%; height: 100%;
   margin: auto;
   background: grey;
 
+  .top-buttons{
+    width: auto;
+    margin: 0 auto 20px;
 
-  &>button{
-    border: 1px solid black; border-radius: 3px;
-    padding: 5px;
-    display: block;
-    width: 120px; margin: 0 auto 20px;
-    background: lightskyblue;
-    cursor: pointer;
+    &>button{
+      border: 1px solid black; border-radius: 3px;
+      padding: 5px;
+      display: inline-block;
+      width: 120px; height: 30px;
+      background: lightskyblue;
+      cursor: pointer;
+    }
   }
+
   ul{
-    width: 75%;
+    width: 90%;
     margin: 0 auto;
   }
   li{
     display: grid; 
-    grid-template-columns: 100px 100px 100px auto 150px 150px; 
-    grid-template-areas: "div1 div2 div3 . btn1 btn2";
+    grid-template-columns: 50px 100px 100px 100px auto 150px; 
+    grid-template-areas: "btn1 div1 div2 div3 . btn2";
     border: 1px solid white;
     div:nth-of-type(1){
       grid-area: div1;
@@ -65,10 +79,13 @@ const Content = styled(ContentPosed)`
       border-right: 1px solid lightgrey;
     }
     button:nth-of-type(1){
+      font-size: 1.5em;
       grid-area: btn1;
+      border: 0;
     }
     button:nth-of-type(2){
       grid-area: btn2;
+      border: 0;
     }
   }
 `;
@@ -77,6 +94,7 @@ const HeaderPosed = posed.h2({
   closed: { y: 20, opacity: 0 }
 })
 const Header = styled(HeaderPosed)`
+  line-height: 30px;
   text-align: center;
   margin: 20px 0;
 `;
@@ -101,11 +119,15 @@ const Item = posed.li({
   open: { y: 0, opacity: 1 },
   closed: { y: 20, opacity: 0 }
 })
+const AddItem = styled.button`
 
+`;
 
 const AdminCategoryModal = ({modal, setModalOpen}) => {
 
   const [isOpen, setIsOpen] = React.useState(false);
+  const [editModalOpen, setEditModalOpen] = React.useState(false);
+  const [editModalContent, setEditModalContent] = React.useState(null);
 
   React.useEffect(()=>{
     setIsOpen(true);
@@ -134,29 +156,54 @@ const AdminCategoryModal = ({modal, setModalOpen}) => {
     }
   ]
 
-  console.log(isOpen);
+  const closeModal = () => {
+    setIsOpen(false);
+    setTimeout(()=>{
+      setModalOpen(false);
+    }, 500)
+  }
+
+  const openEditModal = (item) => {
+    setEditModalContent(item);
+    setEditModalOpen(true);
+  }
 
   return (
 
 
-    <Main>
+    <Main pose={isOpen ? "open" : "closed"}>
+      {editModalOpen && <EditItemModal editModalContent={editModalContent} setEditModalOpen={setEditModalOpen} />}
       <Content pose={isOpen ? "open" : "closed"}>
 
         <Header>Category: {modal.name}</Header>
-        <CloseButton onClick={()=>setModalOpen(false)}>Close <i className="fa fa-times"></i></CloseButton>
+        <div className="top-buttons">
+          <CloseButton onClick={closeModal}>Cancel<i className="fa fa-times"></i></CloseButton>
+          <CloseButton onClick={closeModal}>Apply changes<i className="fa fa-check"></i></CloseButton>
+        </div>
 
         <ItemList pose={isOpen ? "open" : "closed"}>
+
+          <Item>
+            <button></button>
+            <div>name</div>
+            <div>price</div>
+            <div>inStock</div>
+            <button></button>
+          </Item>
+
           {tempDB.map((item,id) =>
             <Item key={id}>
+              <button onClick={()=>openEditModal(item)}><i className="fa fa-cog"></i></button>
               <div>{item.name}</div>
               <div>{item.price}</div>
               <div>{item.inStock}</div>
-              <button>Edit</button>
               <button>Delete</button>
             </Item>
           )}
+        <AddItem>Add new item</AddItem>
         </ItemList>
 
+        
       </Content>
     </Main>
   )
