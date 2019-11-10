1ce8cbdbb2c4113ea481909b53df9b53bf595414
klauza <klauza.uk@gmail.com>
2019-11-10 22:21:27 +0000

    create logic to set token

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,11 @@
+// passing token to global header
+import axios from 'axios';
+const setAuthToken = token => {
+  if(token){
+    axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+  }
+}
+
+export default setAuthToken;
